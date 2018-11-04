97957a62f7737b7c513ec9d74f341e99a9953424
Rawipol Wajanavisit <oakraw@gmail.com>
2018-11-04 09:03:55 +0700

    add Calculator core logic

---
+++ app/src/main/java/com/example/calculator/CalculatorCore.kt
@@ -0,0 +1,27 @@
+package com.example.calculator
+
+object CalculatorCore {
+    fun plus (a: Int, b: Int): Int {
+        return 0
+    }
+
+    fun minus (a: Int, b: Int): Int {
+        return 0
+    }
+
+    fun divide (a: Int, b: Int): Int {
+        return 0
+    }
+
+    fun multiply (a: Int, b: Int): Int {
+        return 0
+    }
+
+    fun square (a: Int): Int {
+        return 0
+    }
+
+    fun root(a: Int): Int {
+        return 0
+    }
+}
